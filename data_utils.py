5b27fac2c1448c05c4e4536f4786c11bccc0d08c
wwz58 <2020354644@qq.com>
2019-11-26 09:21:39 +0800

    add vh_bert data_utils

--- data_utils.py
+++ data_utils.py
@@ -18,11 +18,13 @@ def build_tokenizer(fnames, max_seq_len, dat_fname):
     else:
         text = ''
         for fname in fnames:
-            fin = open(fname, 'r', encoding='utf-8', newline='\n', errors='ignore')
+            fin = open(fname, 'r', encoding='utf-8',
+                       newline='\n', errors='ignore')
             lines = fin.readlines()
             fin.close()
             for i in range(0, len(lines), 3):
-                text_left, _, text_right = [s.lower().strip() for s in lines[i].partition("$T$")]
+                text_left, _, text_right = [
+                    s.lower().strip() for s in lines[i].partition("$T$")]
                 aspect = lines[i + 1].lower().strip()
                 text_raw = text_left + " " + aspect + " " + text_right
                 text += text_raw + " "
@@ -49,7 +51,8 @@ def build_embedding_matrix(word2idx, embed_dim, dat_fname):
         embedding_matrix = pickle.load(open(dat_fname, 'rb'))
     else:
         print('loading word vectors...')
-        embedding_matrix = np.zeros((len(word2idx) + 2, embed_dim))  # idx 0 and len(word2idx)+1 are all-zeros
+        # idx 0 and len(word2idx)+1 are all-zeros
+        embedding_matrix = np.zeros((len(word2idx) + 2, embed_dim))
         fname = './glove.twitter.27B/glove.twitter.27B.' + str(embed_dim) + 'd.txt' \
             if embed_dim != 300 else './glove.42B.300d.txt'
         word_vec = _load_word_vec(fname, word2idx=word2idx)
@@ -100,7 +103,8 @@ class Tokenizer(object):
             text = text.lower()
         words = text.split()
         unknownidx = len(self.word2idx)+1
-        sequence = [self.word2idx[w] if w in self.word2idx else unknownidx for w in words]
+        sequence = [self.word2idx[w]
+                    if w in self.word2idx else unknownidx for w in words]
         if len(sequence) == 0:
             sequence = [0]
         if reverse:
@@ -114,7 +118,8 @@ class Tokenizer4Bert:
         self.max_seq_len = max_seq_len
 
     def text_to_sequence(self, text, reverse=False, padding='post', truncating='post'):
-        sequence = self.tokenizer.convert_tokens_to_ids(self.tokenizer.tokenize(text))
+        sequence = self.tokenizer.convert_tokens_to_ids(
+            self.tokenizer.tokenize(text))
         if len(sequence) == 0:
             sequence = [0]
         if reverse:
@@ -123,6 +128,72 @@ class Tokenizer4Bert:
 
 
 class ABSADataset(Dataset):
+    def __init__(self, fname, tokenizer, max_len=80, debug=False):
+        fin = open(fname, 'r', encoding='utf-8', newline='\n', errors='ignore')
+        lines = fin.readlines()
+        fin.close()
+
+        all_data = []
+        for i in range(0, len(lines), 3):
+            if i == 300:
+                print('')
+            if debug:
+                if i >= 3 * 100:
+                    break
+            text_left, _, text_right = [s.lower().strip()
+                                        for s in lines[i].partition("$T$")]
+            aspect = lines[i + 1].lower().strip()
+            polarity = lines[i + 2].strip()
+
+            left_input_ids = tokenizer.convert_tokens_to_ids(
+                tokenizer.tokenize(text_left))
+            right_input_ids = tokenizer.convert_tokens_to_ids(
+                tokenizer.tokenize(text_right))
+            aspect_ids = tokenizer.convert_tokens_to_ids(
+                tokenizer.tokenize(aspect))
+
+            while len(left_input_ids) + len(right_input_ids) > max_len - 2 - len(aspect_ids):
+                if len(left_input_ids) > len(right_input_ids):
+                    left_input_ids.pop(0)
+                else:
+                    right_input_ids.pop()
+
+            pos = np.array([len(left_input_ids), len(
+                left_input_ids) + len(aspect_ids)])
+            cls_id, sep_id = [101], [102]
+            input_ids = cls_id + left_input_ids + aspect_ids + right_input_ids + sep_id
+            attention_mask = [1]*len(input_ids)
+            paddings = [0] * (max_len - len(input_ids))
+            input_ids += paddings
+            attention_mask = attention_mask+paddings
+
+            while len(left_input_ids) + len(right_input_ids) > max_len - 3 - 2*len(aspect_ids):
+                if len(left_input_ids) > len(right_input_ids):
+                    left_input_ids.pop(0)
+                else:
+                    right_input_ids.pop()
+            text_asp_ids = cls_id + left_input_ids + aspect_ids + \
+                right_input_ids + sep_id + aspect_ids + sep_id
+            text_asp_att_mask = [1]*len(text_asp_ids)
+            paddings = [0] * (max_len - len(text_asp_ids))
+            text_asp_ids += paddings
+            text_asp_att_mask += paddings
+            data = {'input_ids': np.array(input_ids, dtype='int64'),
+                    'attention_mask': np.array(attention_mask, dtype='int64'),
+                    'pos': pos, 'polarity': int(polarity) + 1,
+                    'text_asp_ids': np.array(text_asp_ids, dtype='int64'),
+                    'text_asp_att_mask': np.array(text_asp_att_mask, dtype='int64')}
+            all_data.append(data)
+            self.data = all_data
+
+    def __getitem__(self, index):
+        return self.data[index]
+
+    def __len__(self):
+        return len(self.data)
+
+
+class _ABSADataset(Dataset):
     def __init__(self, fname, tokenizer):
         fin = open(fname, 'r', encoding='utf-8', newline='\n', errors='ignore')
         lines = fin.readlines()
@@ -130,28 +201,40 @@ class ABSADataset(Dataset):
 
         all_data = []
         for i in range(0, len(lines), 3):
-            text_left, _, text_right = [s.lower().strip() for s in lines[i].partition("$T$")]
+            text_left, _, text_right = [s.lower().strip()
+                                        for s in lines[i].partition("$T$")]
             aspect = lines[i + 1].lower().strip()
             polarity = lines[i + 2].strip()
 
-            text_raw_indices = tokenizer.text_to_sequence(text_left + " " + aspect + " " + text_right)
-            text_raw_without_aspect_indices = tokenizer.text_to_sequence(text_left + " " + text_right)
+            text_raw_indices = tokenizer.text_to_sequence(
+                text_left + " " + aspect + " " + text_right)
+            text_raw_without_aspect_indices = tokenizer.text_to_sequence(
+                text_left + " " + text_right)
             text_left_indices = tokenizer.text_to_sequence(text_left)
-            text_left_with_aspect_indices = tokenizer.text_to_sequence(text_left + " " + aspect)
-            text_right_indices = tokenizer.text_to_sequence(text_right, reverse=True)
-            text_right_with_aspect_indices = tokenizer.text_to_sequence(" " + aspect + " " + text_right, reverse=True)
+            text_left_with_aspect_indices = tokenizer.text_to_sequence(
+                text_left + " " + aspect)
+            text_right_indices = tokenizer.text_to_sequence(
+                text_right, reverse=True)
+            text_right_with_aspect_indices = tokenizer.text_to_sequence(
+                " " + aspect + " " + text_right, reverse=True)
             aspect_indices = tokenizer.text_to_sequence(aspect)
             left_context_len = np.sum(text_left_indices != 0)
             aspect_len = np.sum(aspect_indices != 0)
-            aspect_in_text = torch.tensor([left_context_len.item(), (left_context_len + aspect_len - 1).item()])
+            aspect_in_text = torch.tensor(
+                [left_context_len.item(), (left_context_len + aspect_len - 1).item()])
             polarity = int(polarity) + 1
 
-            text_bert_indices = tokenizer.text_to_sequence('[CLS] ' + text_left + " " + aspect + " " + text_right + ' [SEP] ' + aspect + " [SEP]")
-            bert_segments_ids = np.asarray([0] * (np.sum(text_raw_indices != 0) + 2) + [1] * (aspect_len + 1))
-            bert_segments_ids = pad_and_truncate(bert_segments_ids, tokenizer.max_seq_len)
+            text_bert_indices = tokenizer.text_to_sequence(
+                '[CLS] ' + text_left + " " + aspect + " " + text_right + ' [SEP] ' + aspect + " [SEP]")
+            bert_segments_ids = np.asarray(
+                [0] * (np.sum(text_raw_indices != 0) + 2) + [1] * (aspect_len + 1))
+            bert_segments_ids = pad_and_truncate(
+                bert_segments_ids, tokenizer.max_seq_len)
 
-            text_raw_bert_indices = tokenizer.text_to_sequence("[CLS] " + text_left + " " + aspect + " " + text_right + " [SEP]")
-            aspect_bert_indices = tokenizer.text_to_sequence("[CLS] " + aspect + " [SEP]")
+            text_raw_bert_indices = tokenizer.text_to_sequence(
+                "[CLS] " + text_left + " " + aspect + " " + text_right + " [SEP]")
+            aspect_bert_indices = tokenizer.text_to_sequence(
+                "[CLS] " + aspect + " [SEP]")
 
             data = {
                 'text_bert_indices': text_bert_indices,
@@ -177,3 +260,11 @@ class ABSADataset(Dataset):
 
     def __len__(self):
         return len(self.data)
+
+
+if __name__ == "__main__":
+    tok = BertTokenizer.from_pretrained(
+        '/mnt/sda1/bert/uncased_L-12_H-768_A-12')
+    d = ABSADataset(
+        '/mnt/sda1/wwz/absa/ABSA-PyTorch/datasets/semeval14/Restaurants_Train.xml.seg', tok, 80)
+    print(d[0])
